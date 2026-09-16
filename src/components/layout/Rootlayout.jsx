95dacd026d56a4a42943f10823112d4bcee49cab
mdwasim <wasimmahamodraza@gmail.com>
2026-09-16 11:50:48 +0600

    page show on top

--- src/components/layout/Rootlayout.jsx
+++ src/components/layout/Rootlayout.jsx
@@ -1,9 +1,15 @@
-import React from "react";
-import { Outlet } from "react-router";
+import React, { useEffect } from "react";
+import { Outlet, useLocation } from "react-router";
 import Header from "./Header";
 import MobileNav from "./MobileNav";
 
 const Rootlayout = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <main>
       <Header />
